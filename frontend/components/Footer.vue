1d0fa4b0272794a83435c389ca658bdd274a92b9
MiyRon <miyron.my@yandex.kz>
2021-05-15 11:24:10 +0600

    footer update

--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -3,15 +3,26 @@
     <footer class="bg-black w-auto h-80 text-white justify-around flex items-start p-10">
   <div class="flex justify-start items-start flex-col w-full mr-20" >
     <div class = "pb-2 uppercase" >About P.A.D</div>
-    <span class="w-full bg-white h-px"></span>
+    <span class="w-full bg-white h-px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam eligendi quas fuga delectus molestias voluptatibus veritatis soluta dolor, architecto eos odit consectetur est provident quidem minima, fugiat pariatur ullam quae.</span>
   </div>
   <div class="flex justify-start items-start flex-col w-full mr-20" >
     <div class = " pb-2 uppercase">Iformation</div>
-    <span class="w-full bg-white h-px"></span>
+    <span class="w-full bg-white h-px">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio laborum aliquam sunt magnam 
+      2021
+    </span>
   </div>
   <div class="flex justify-start items-start flex-col w-full mr-20" >
     <div class = "pb-2 uppercase">Contact Us</div>
-    <span class="w-full bg-white h-px"></span>
+    <span class="w-full bg-white h-px">
+      <ul>
+        <li>Office</li>
+        <li>Number: +7800-555-35-35</li>
+        <li>Email: pad.@padovich.com</li>
+        <li>Instagram: @pad</li>
+        <li>FaceBook: Pad Store</li>
+      </ul>
+    </span>
   </div>
 </footer>
   </div>
