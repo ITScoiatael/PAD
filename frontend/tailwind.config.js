08600bd253257be5a20be1da377e17d711e60d67
Мырзалиев Данияр <@lcoact.itstep>
2021-05-17 16:30:30 +0600

    Minor changes in main page and login page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,16 +18,14 @@ module.exports = {
         '50%': '50%',
         '25%': '25%',
         '20%': '20%',
+        '18%': '18%',
         '15%': '15%',
       },
       backgroundColor: theme => ({
         ...theme('colors'),
       }),
       backgroundPosition: {
-       'center-right': 'center right 25rem',
-      },
-      fontFamily: {
-        'gilroy': ['Gilroy'],
+        'center-right': 'center right 22%',
       },
       colors: {
         emerald: colors.emerald,
@@ -35,11 +33,33 @@ module.exports = {
         primary: '#CDC1C1',
         'light-blue': colors.lightBlue,
         cyan: colors.cyan,
+        yellow: {
+          'bgcol': '#F6F4EE',
+          'buycol': '#E1E0DC',
+          'bgbuycol': '#E5E5E5'
+        }
+      },
+      screen: {
+        'lg': '1024px'
+      },
+      placeholderColor: {
+        'white': '#fff'
+      },
+      fontFamily: {
+        'gilroy': 'Gilroy',
       },
       w: {
         '10': '10px',
         '20': '20px',
         '100p': '100%',
+      },
+      width: {
+        '100': '100px',
+        '700': '700px',
+        '1': '1px',
+      },
+      height: {
+        '400': '400px'
       },
       h: {
         '10': '10px',
@@ -55,7 +75,10 @@ module.exports = {
       md: {
         'md5': '5px',
         'md10': '10px'
-      }
+      },
+      rounded: {
+        'md': "10%",
+      },
     },
   },
   variants: {
