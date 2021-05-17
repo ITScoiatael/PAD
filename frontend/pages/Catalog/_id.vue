1703cc2ecda997aefae29d8f3e1c895b291f248d
Мырзалиев Данияр <@lcoact.itstep>
2021-05-17 17:19:58 +0600

    Changed catalog frintend

--- frontend/pages/Catalog/_id.vue
+++ frontend/pages/Catalog/_id.vue
@@ -1,59 +1,68 @@
 <script>
-import gql from 'graphql-tag';
+import gql from "graphql-tag";
 
 const Products_QUERY = gql`
-    query($id: ID!) {
-        Products(category_id: $id){
-            id
-            name
-            description
-            fabric
-            manufacturer
-            images{
-                id
-                url
-            }
-            sub_products{
-                id
-                price
-                size
-                color
-                amount
-            }
-        }
+  query($id: ID!) {
+    Products(category_id: $id) {
+      id
+      name
+      description
+      fabric
+      manufacturer
+      images {
+        id
+        url
+      }
+      sub_products {
+        id
+        price
+        size
+        color
+        amount
+      }
     }
-`
+  }
+`;
 
 export default {
-    layout: 'CatalogLayout',
-    apollo: {
-        Products: {
-            query: Products_QUERY,
-            variables() {
-                return{
-                    id: this.$route.params.id
-                }
-            }
-        }, 
-    },
-    methods:{
-        ClickBy(Product){
-          this.$router.push('/Buy/'+ Product.id)
-        },
-    },
-
-}
+  layout: "CatalogLayout",
+  apollo: {
+    Products: {
+      query: Products_QUERY,
+      variables() {
+        return {
+          id: this.$route.params.id
+        };
+      }
+    }
+  },
+  methods: {
+    ClickBy(Product) {
+      this.$router.push("/Buy/" + Product.id);
+    }
+  }
+};
 </script>
 
+
 <template>
-    <div>
-        <div class = " border-red-300 flex flex-col text-lg mb-60">
-            <div class = "w-auto  flex items-center justify-between px-40 py-20 ">
-                <div class = " h-80 w-72" v-for="(Product, idx) in Products" :key="idx"> 
-                    <div class = "border-2 border-black w-full h-80"><img :src="`http://localhost:8080/static/`+Product.images[0].url" class="w-444 h-640" alt="если ты это видишь то что то с сервером"></div>
-                    <button @click.prevent="ClickBy(Product)" class = "mt-6 w-full h-8 px-3 flex justify-around items-center bg-gray-600 text-white hover:text-gray-300">price: ${{Product.sub_products[0].price}}</button>
-                </div>
-            </div>
-        </div>
+  <div class="border-red-300 flex flex-col text-lg mb-60 items-center justify-around">
+    <div class="w-auto lg:flex lg:items-center lg:justify-center py-20 flex-wrap">
+      <div
+        class="h-80 w-72 mx-20 flex items-center justify-center flex-col lg:mt-20 mt-24"
+        v-for="(Product, idx) in Products"
+        :key="idx"
+      >
+        <img
+          :src="`http://localhost:8080/static/`+Product.images[0].url"
+          class="border-2 border-black"
+          alt="если ты это видишь то что то с сервером"
+        />
+        <button
+          @click.prevent="ClickBy(Product)"
+          class="mt-2 w-full h-8 px-3 flex justify-center items-center bg-gray-600 text-white hover:text-gray-300"
+        >price: ${{Product.sub_products[0].price}}</button>
+      </div>
     </div>
+  </div>
 </template>
