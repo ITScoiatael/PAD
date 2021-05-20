0cd3c353622828d4ddfb6192fe101bf14da659b5
MiyRon-Code <miyron.my@yandex.kz>
2021-05-20 16:19:16 +0600

    sorry

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -1,27 +1,7 @@
 <template>
-<<<<<<< HEAD
-    <section>
-        asdasd
-    </section>
-<<<<<<< HEAD
-</template>
-<script>
-export default{
-    created(){
-        alert('qwe');
-        console.log('qwe')
-        console.log(this.env)
-    }
-}
-</script>
-=======
   <section>
     <div class="min-h-screen bg-GrayImage bg-no-repeat">
         
     </div>
   </section>
 </template>
->>>>>>> 73dbf1bc4f29960b2dbd8388c469aa81893fa41b
-=======
-</template>
->>>>>>> parent of 326cb16 (update catalog component)
