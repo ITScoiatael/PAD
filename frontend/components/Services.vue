7d10dc7a997ae8919560bd67c0012eea9ba47200
MiyRon-Code <miyron.my@yandex.kz>
2021-05-20 16:34:19 +0600

    update catalog and home pages

--- frontend/components/Services.vue
+++ frontend/components/Services.vue
@@ -3,10 +3,10 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 gap-y-10 gap-x-5 lg:py-40 lg:px-60">
       <div class="px-5 py-5">
         <div>
-          <p class="mb-1 font-bold text-white text-center flex justify-center">Express-Delivery</p>
-          <p
-            class="text-xs text-white text-center"
-          >Free delivery throughout Kazakhstan using the P.A.D Economy method or when purchasing goods for a certain amount, according to the conditions for each city individually. When paying for an order on the website by credit card, delivery by any service is free.</p>
+          <p class="mb-1 font-bold text-white text-center flex justify-center text-4xl">Express-Delivery</p>
+          <div
+            class="text-xs text-white text-center text-2xl py-7"
+          >Free delivery throughout Kazakhstan using the P.A.D Economy method or when purchasing goods for a certain amount, according to the conditions for each city individually. When paying for an order on the website by credit card, delivery by any service is free.</div>
         </div>
       </div>
 
@@ -31,10 +31,10 @@
       <div class="px-5 py-5">
         <div>
           <p
-            class="mb-1 font-bold text-white text-center flex justify-center"
+            class="mb-1 font-bold text-white text-center flex justify-center text-4xl"
           >Trying on clothes before buying</p>
           <p
-            class="text-xs text-white text-center"
+            class="text-xs text-white text-center text-2xl py-7"
           >The P.A.D online store gives you the opportunity to try on clothes and shoes before paying for the order. Pay only for what suits you and you like it. Also, you always have a whole year to return an unsuitable product by mail or courier upon receipt of the next order.</p>
         </div>
       </div>
@@ -42,10 +42,10 @@
       <div class="px-5 py-5">
         <div>
           <p
-            class="mb-1 font-bold text-white text-center flex justify-center"
+            class="mb-1 font-bold text-white text-center flex justify-center text-4xl"
           >Convenient payment methods</p>
           <p
-            class="text-xs text-white text-center"
+            class="text-xs text-white text-center text-2xl py-7"
           >You can pay for purchases not only in cash, but also by credit card. All P.A.D Express sales representatives have a mobile terminal for payment with cards. When paying for an order on the website by credit card, delivery by any service is free.</p>
         </div>
       </div>
