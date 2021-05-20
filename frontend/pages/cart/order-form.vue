82305d71b31046ab1fdbef77b58697aab81dfeea
MiyRon-Code <miyron.my@yandex.kz>
2021-05-20 17:01:23 +0600

    order-form

---
+++ frontend/pages/cart/order-form.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="justify-center items-center flex flex-col">
+    <div class="py-4 sm:py-20 lg:py-40 h-full w-full justify-center items-center flex">
+      <div
+        class="w-300 sm:w-500 lg:w-750 bg-white border-2 rounded-xl border-black h-full flex flex-col justify-center items-center">
+        <div class=" my-6 w-full justify-center items-center flex">
+          <div class="border-2 border-black w-40 h-40"></div>
+        </div>
+        <div class="w-full px-3">
+          <input type="text"
+            class="w-full mb-3 text-lg border-2 border-gray-400 rounded-md py-2 px-2 transition duration-150 focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 focus:outline-none focus:placeholder-transparent"
+            placeholder="Name">
+          <input type="text"
+            class="w-full mb-3 text-lg border-2 border-gray-400 rounded-md py-2 px-2 transition duration-150 focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 focus:outline-none focus:placeholder-transparent"
+            placeholder="Email">
+          <input type="text"
+            class="w-full mb-3 text-lg border-2 border-gray-400 rounded-md py-2 px-2 transition duration-150 focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 focus:outline-none focus:placeholder-transparent"
+            placeholder="Phone">
+          <input type="text"
+            class="w-full mb-3 text-lg border-2 border-gray-400 rounded-md py-2 px-2 transition duration-150 focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 focus:outline-none focus:placeholder-transparent"
+            placeholder="Address">
+          <input type="text"
+            class="w-full mb-3 text-lg border-2 border-gray-400 rounded-md py-2 px-2 transition duration-150 focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 focus:outline-none focus:placeholder-transparent"
+            placeholder="Region">
+          <input type="text"
+            class="w-full text-lg border-2 border-gray-400 rounded-md py-2 px-2 transition duration-150 focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 focus:outline-none focus:placeholder-transparent"
+            placeholder="CC_number">
+        </div>
+
+        <div class="w-full py-3 justify-center items-center flex">
+          <button
+            class="bg-black text-lg text-white rounded-md w-200 py-1 transition duration-100 focus:outline-none focus:ring-2 focus:ring-black focus:ring-opacity-50">Log
+            In</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
